15b53cd671f1991b20f4d5e29ca5fc67b12417c5
yhonder710 <agueropibe1@email.com>
2025-05-21 17:23:12 -0400

    feat: se agrego la funcion favoritos a la card de busqueda

--- src/components/molecula/CardsBusqueda.tsx
+++ src/components/molecula/CardsBusqueda.tsx
@@ -1,19 +1,22 @@
-import { PokemonsList } from "../../intefaces/pageApi";
 import { TbPokeball } from "react-icons/tb";
 import { Heart } from '../atomo/Heart';
 import './css/cardsBusqueda.css'
+import { useFavoritos } from "../../hooks/logicaFavoritos";
+import { PokemonsList } from "../../interfaces/pageApi";
 
 
 interface Props {
-  pokemons: PokemonsList | undefined
+  pokemons: PokemonsList
 }
 
 export function CardsBusqueda({ pokemons }: Props) {
+  const { handleFavoriteClick, like } = useFavoritos(pokemons)
+
   return (
     <article className='boxArticle'>
       <header className='boxHeader'>
         <span className='iconPoke'><TbPokeball className='pokebola' /> {pokemons?.id}</span>
-        <Heart />
+        <Heart like={like} functionPokemons={() => handleFavoriteClick(pokemons)} />
       </header>
       <img key={pokemons?.id} src={pokemons?.sprites.other?.dream_world.front_default} alt="img pokemon" />
       <footer>
